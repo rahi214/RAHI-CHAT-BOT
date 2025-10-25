df726b8989f54409e75da49bdd17ddb4b435c739
Rahi <rahi50000000@gmail.com>
2025-10-26 00:59:30 +0600

    Update Bot.js

--- Script/commands/Bot.js
+++ Script/commands/Bot.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "bot",
   version: "1.0.0",
   hasPermission: 0,
-  credits: "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐈𝐬𝐥𝐚𝐦",
+  credits: "𝐑𝐚𝐡𝐢 𝐂𝐡𝐨𝐰𝐝𝐡𝐮𝐫𝐲",
   description: "Random fun reply when someone says Bot",
   commandCategory: "Fun",
   usages: "[Bot]",
